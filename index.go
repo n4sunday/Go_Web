913d43e7db02393aac310f2014adffa2eb3a73a5
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-16 04:07:32 +0700

    Gorilla_nux

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "github.com/gorilla/mux"
 )
 func main() {
     userDB:=map[string]int{
@@ -9,18 +10,17 @@ func main() {
         "Ding":23,
         "Noey":25,
     }
-
-    http.HandleFunc("/",func(res http.ResponseWriter, req *http.Request){
+    router:=mux.NewRouter()
+    router.HandleFunc("/",func(res http.ResponseWriter, req *http.Request){
         fmt.Fprintf(res,"Server Go Ready")
     })
 
-    http.HandleFunc("/user/",func(res http.ResponseWriter, req *http.Request){
-        name:=req.URL.Path[len("/user/"):]
+    router.HandleFunc("/user/{name}",func(res http.ResponseWriter, req *http.Request){
+        vars:=mux.Vars(req)
+        name:=vars["name"]
         age:=userDB[name]
         fmt.Fprintf(res,"My Name is %s %d year old",name,age)
-    })
-
-
-
-    http.ListenAndServe(":8080",nil)
+    }).Methods("GET")
+    
+    http.ListenAndServe(":8080",router)
 }
