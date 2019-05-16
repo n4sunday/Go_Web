dafffe94461951bf8be401efa35afe03e878d332
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-16 23:09:55 +0700

    html template

--- index.go
+++ index.go
@@ -1,26 +1,20 @@
 package main
 import (
-    "fmt"
     "net/http"
     "github.com/gorilla/mux"
+    "html/template"
 )
+type Product struct{
+    Name string
+    Price int
+}
 func main() {
-    userDB:=map[string]int{
-        "Sun":21,
-        "Ding":23,
-        "Noey":25,
-    }
+    var templates = template.Must(template.ParseFiles("index.html"))
     router:=mux.NewRouter()
     router.HandleFunc("/",func(res http.ResponseWriter, req *http.Request){
-        fmt.Fprintf(res,"Server Go Ready")
+        myProduct := Product{"นมสด",250}
+        templates.ExecuteTemplate(res,"index.html",myProduct)
     })
 
-    router.HandleFunc("/user/{name}",func(res http.ResponseWriter, req *http.Request){
-        vars:=mux.Vars(req)
-        name:=vars["name"]
-        age:=userDB[name]
-        fmt.Fprintf(res,"My Name is %s %d year old",name,age)
-    }).Methods("GET")
-    
     http.ListenAndServe(":8080",router)
 }
